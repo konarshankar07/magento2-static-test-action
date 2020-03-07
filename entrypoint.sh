1652bb0b256fdffd057b9232ab8c76594a41ca9a
konarshankar07 <konar.shankar2013@gmail.com>
2020-03-07 12:10:07 +0530

    vendor directory ignored

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Running Magento2 PHP CodeSniffer"
 composer require --dev magento/magento-coding-standard
 vendor/bin/phpcs --config-set installed_paths ../../magento/magento-coding-standard/
-vendor/bin/phpcs --standard=Magento2 ./
+vendor/bin/phpcs --ignore=*/vendor/* --standard=Magento2 ./
